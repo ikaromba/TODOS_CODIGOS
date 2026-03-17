593b7befa885e8b87e672a6bc0b9e2e6485ebb5a
crosmerdacu1 <fanimada0@gmail.com>
2026-03-17 14:10:39 -0300

    .

---
+++ Aulas/Aula4_Anderi/1/Leitor_Codigo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Leritor_par_Impar
+{
+    class Progam
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indentificador de N inteiro se é imapar ou par");
+            Console.WriteLine("Digite o numero: ");
+            int N = int.Parse(Console.ReadLine());
+            Caculo_Par_Impar(N);
+
+        }
+
+                static void Caculo_Par_Impar(int N)
+        {
+            if (N % 2 == 0)
+            {
+                Console.WriteLine("O NUMERO É PAR");
+            }else
+            {
+                Console.WriteLine("O NUMERO É IMPAR");
+            }
+        }
+    }
+}
