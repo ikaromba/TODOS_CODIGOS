ea66b5a97ddac3eb1c0f00c54f08c4d9c2d2e212
crosmerdacu1 <fanimada0@gmail.com>
2026-03-17 08:29:42 -0300

    new

---
+++ teste_b/Bird_Question.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+class Program
+{
+       static void Passaro_Domestico()
+{
+if (Reposta("É verde e fala? (s/n): "))
+ {
+  Console.WriteLine("Resultado: Papagaio");
+  } else if (Reposta("Tem uma mancha laranja na bochecha? (s/n): "))
+    {
+     Console.WriteLine("Resultado: Calopsita");
+    }else if (Reposta("É pequeno e colorido? (s/n): "))
+    {
+        Console.WriteLine("Resultado: Periquito Australiano");
+    }else
+    {
+        Console.WriteLine("Resultado: Pássaro desconhecido");
+    }
+}
+
+     static void Passaro_Selvagem()
+{
+    Console.WriteLine("Ele é um passaro selvagem");
+    if (Reposta("É um predador noturno que imata um tronco? (s/n): "))
+ {
+  Console.WriteLine("Resultado: Urutau");
+  }else 
+    {
+      if (Reposta("Ele tem olhos grandes? (s/n): "))
+        {
+            Console.WriteLine("Resultad: Coruja");
+        }else if (Reposta("Ele é conhecido por dar rasante no ar? (s/n): "))
+        {
+            Console.WriteLine("Resultado: Falcao");
+        }else
+        {
+            Console.WriteLine("Resultado: Passaro indefinido");
+        }
+    }
+}
+      static bool Reposta(string Pergunta)
+{    
+    Console.WriteLine( Pergunta);
+    string resposta = Console.ReadLine().ToLower();//O programa pausa, lê o que o usuário digitou e já converte para minúsculas (para evitar erros com "SIM" ou "Sim").
+    return (resposta == "s"); //Se for S a resposta enviara true para a função boll 
+}
+    static void Main()
+    {
+    The_Big_Bird();
+    Console.WriteLine("-------- Identificador de Pássaros --------");
+//sei que a palavra "reposta" ta errada
+    if (Reposta("O pássaro é de estimação? (s/n): "))
+        {
+            Passaro_Domestico();
+        }else
+        {
+            Passaro_Selvagem();
+        }
+    }
+
+    static void The_Big_Bird()
+    {
+    String bird = (@"
+ -BIRD
+    ____       _____   ____
+   | _  \  () | _  || |  _ \
+   ||_| /  || ||_| // | | | |
+   | _ |   || |   //  | | | |
+   ||_| \  || | |\ \  | | | |
+   |____/  || | | \ \ |____/
+                       
+                    /_/ \            
+                  o/ o   \____      ///
+                  <     ______\___ ////
+   _               \  /        \____  /
+  \ \______         \  \           _ \
+   \ _  __ \          \ _ \________\
+Ika  /_/  \ \___________ // //_________
+           \_________________________  \
+                                     |  |");
+ Console.WriteLine("{0}", bird);
+    }
+}
---
+++ teste_b/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+               String bird = (@"
+ -BIRD
+    ____       _____   ____
+   | _  \  () | _  || |  _ \
+   ||_| /  || ||_| // | | | |
+   | _ |   || |   //  | | | |
+   ||_| \  || | |\ \  | | | |
+   |____/  || | | \ \ |____/
+                       
+                    /_/ \            
+                  o/ o   \____      ///
+                  <     ______\___ ////
+   _               \  /        \____  /
+  \ \______         \  \           _ \
+   \ _  __ \          \ _ \________\
+Ika  /_/  \ \___________ // //_________
+           \_________________________  \
+                                     |  |");
+
+        Console.WriteLine("{0}", bird);
+        Console.WriteLine("--- Identificador de Pássaros ---");
+
+        Console.Write("O pássaro é de estimação? (sim/nao): ");
+        string pet = Console.ReadLine().ToLower();
+//Passaralho();
+
+        if (pet == "sim")
+        {
+            Console.Write("É verde e fala? (sim/nao): ");
+            if (Console.ReadLine().ToLower() == "sim")
+            {
+                Console.WriteLine("Resultado: Papagaio");
+            }
+            else
+            {
+                Console.Write("Tem uma mancha laranja na bochecha? (sim/nao): ");
+                if (Console.ReadLine().ToLower() == "sim")
+                {
+                    Console.WriteLine("Resultado: Calopsita");
+                }
+                else
+                {
+                    Console.Write("É pequeno e colorido? (sim/nao): ");
+                    if (Console.ReadLine().ToLower() == "sim")
+                    {
+                        Console.WriteLine("Resultado: Periquito Australiano");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Resultado: Pássaro desconhecido");
+                    }
+                }
+            }
+        }
+        else
+        {
+            Console.Write("É um predador noturno? (sim/nao): ");
+            if (Console.ReadLine().ToLower() == "sim")
+            {
+                Console.Write("Imita um galho/tronco? (sim/nao): ");
+                if (Console.ReadLine().ToLower() == "sim")
+                {
+                    Console.WriteLine("Resultado: Urutau");
+                }
+                else
+                {
+                    Console.WriteLine("Resultado: Coruja ou Bacurau (verifique o tamanho)");
+                }
+            }
+            else
+            {
+                Console.Write("Ele é conhecido por dar rasante no ar? (sim/nao): ");
+                if (Console.ReadLine().ToLower() == "sim")
+                {
+                    Console.WriteLine("Resultado: Gavião");
+                }
+                else
+                {
+                    Console.WriteLine("Resultado: Pássaro");
+                }
+            }
+        }
+        //static void Passaralho()
+        {
+            
+        }
+    }
+}
+
