44abad93eed3420f4551bde9b139cc54a6b2ae9b
crosmerdacu1 <fanimada0@gmail.com>
2026-03-17 09:46:04 -0300

    repertorioa

--- Betas_Interminados/teste_bird/obj/Debug/net9.0/teste_bird.AssemblyInfo.cs
+++ Betas_Interminados/teste_bird/obj/Debug/net9.0/teste_bird.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("teste_bird")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7516042e214c343dfd29ca204fa1eee8e3cdd80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbf47870d937cb33251cb07d85c7717b7c8d635d")]
 [assembly: System.Reflection.AssemblyProductAttribute("teste_bird")]
 [assembly: System.Reflection.AssemblyTitleAttribute("teste_bird")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
