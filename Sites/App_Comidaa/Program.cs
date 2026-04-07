3335024db5055b43b49d6d03084d67c98ae72ce5
crosmerdacu1 <fanimada0@gmail.com>
2026-04-07 10:49:26 -0300

    atu

--- Sites/App_Comidaa/Program.cs
+++ Sites/App_Comidaa/Program.cs
@@ -16,7 +16,7 @@ namespace App_Comidaa
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(mainForm: new Form2());
         }
     }
 }
